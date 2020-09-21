3b53edaeb8851b997da6a706ac173053a9dcdd32
Nahum-png <nahuolvera@gmail.com>
2020-09-21 00:41:53 -0500

    Estructura LinkedList Creada

---
+++ src/uaslp/enginering/labs/list/LinkedList.java
@@ -0,0 +1,50 @@
+package uaslp.enginering.labs.list;
+
+public class LinkedList {
+    private Node Front;
+    private Node Tail;
+
+    public Node getFront() {
+        return Front;
+    }
+
+    public void setFront(Node front) {
+        Front = front;
+    }
+
+    public Node getTail() {
+        return Tail;
+    }
+
+    public void setTail(Node tail) {
+        Tail = tail;
+    }
+
+    public void add(Student student){
+
+    }
+
+    public void delete(Student student){
+
+    }
+
+    public void delete(int index){
+
+    }
+
+    public IteratorArrayList getIterator(){
+
+    }
+
+    public int size(){
+
+    }
+
+    public Student getAt(int index){
+
+    }
+
+    public void insert(Student reference, Student newStudent){
+
+    }
+}
