f49dd198849af65200c9aa8260f5ff7aada0f864
Ivan Uresti <ivan.uresti@gmail.com>
2020-09-21 23:42:24 -0500

    Add unit tests

--- src/uaslp/enginering/labs/list/LinkedList.java
+++ src/uaslp/enginering/labs/list/LinkedList.java
@@ -4,11 +4,24 @@ public class LinkedList {
     private Node Front;
     private Node Tail;
 
-    public class IteratorLinkedList{
+    public enum InsertPosition {
+        BEFORE,
+        AFTER
+    }
+
+    public class Iterator {
         public Node field;
 
-        public Node method(Node node){
+        public Node method(Node node) {
             return (node);
+        }
+
+        public boolean hasNext() {
+            return false;
+        }
+
+        public Student next() {
+            return null;
         }
     }
 
@@ -28,31 +41,31 @@ public class LinkedList {
         Tail = tail;
     }
 
-    public void add(Student student){
+    public void add(Student student) {
 
     }
 
-    public void delete(Student student){
+    public void delete(Student student) {
 
     }
 
-    public void delete(int index){
+    public void delete(int index) {
 
     }
 
-    public IteratorLinkedList getIterator(){
+    public Iterator getIterator() {
         return (null);
     }
 
-    public Integer size(){
+    public Integer size() {
         return (null);
     }
 
-    public Student getAt(int index){
+    public Student getAt(int index) {
         return (null);
     }
 
-    public void insert(Student reference, Student newStudent){
+    public void insert(Student reference, Student newStudent, InsertPosition insertPosition) {
 
     }
 }
--- src/uaslp/enginering/labs/list/Student.java
+++ src/uaslp/enginering/labs/list/Student.java
@@ -2,4 +2,16 @@ package uaslp.enginering.labs.list;
 
 public class Student {
     public String name;
+
+    public Student(String name) {
+        this.name = name;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
 }
